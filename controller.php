e59ca82b91ea602b081434c5eda0de1c8d101743
DanielBr98 <danielbrasild10@hotmail.com>
2021-01-15 21:37:33 -0300

    New update

--- controller.php
+++ controller.php
@@ -1,5 +1,5 @@
 <?php
-require 'Api.php';
+require 'Price.php';
 extract($_POST);
 $array = Price::cost($currency, $price);
 echo "$array[0]|$array[1]|$array[2]|$array[3]|$array[4]|$array[5]|$array[6]";
